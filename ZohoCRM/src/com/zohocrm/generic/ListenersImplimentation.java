7a05cb325c72be94e04f91bc3c6023e7d23ce691
ABC <ABC@DESKTOP-LH46NDJ>
2023-04-11 14:44:11 +0530

    Zoho CRM code

---
+++ ZohoCRM/src/com/zohocrm/generic/ListenersImplimentation.java
@@ -0,0 +1,77 @@
+package com.zohocrm.generic;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenersImplimentation extends Baseclass implements ITestListener 
+{
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestStart(result);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestSuccess(result);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) 
+	{
+		String ran = result.getName();
+		TakesScreenshot t = (TakesScreenshot) driver;
+		File scr = t.getScreenshotAs(OutputType.FILE);
+		File dcr = new File("./screenshot/"+ran+".png");
+		try 
+		{
+			FileUtils.copyFile(scr, dcr);
+		} 
+		catch (IOException e) 
+		{
+
+		}
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestSkipped(result);
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedWithTimeout(result);
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onStart(context);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onFinish(context);
+	}
+	
+	
+
+}
